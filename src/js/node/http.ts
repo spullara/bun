b8299dc3f1c79bef56f55d9fbbd8df4fbd31e486
cirospaciari <ciro.spaciari@gmail.com>
2023-09-13 19:05:53 -0700

    fix http module hanging

--- src/js/node/http.ts
+++ src/js/node/http.ts
@@ -654,7 +654,7 @@ class IncomingMessage extends Readable {
       if (this.#aborted) return;
       if (done) {
         this.push(null);
-        this.destroy();
+        process.nextTick(destroyBodyStreamNT, this);
         break;
       }
       for (var v of value) {
